974e7aef475f0b20650a7ac9e9e35e21b76744d1
Roland Kiss <2021067@dszcberegszaszi.hu>
2024-09-11 10:09:12 +0200

    Add project files.

---
+++ TobbElem/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TobbElem
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int Összeg = 0;
+            List<int> Paros = new List<int>() { };
+            List<int> Paratlan = new List<int>() { };
+            while (Összeg < 100)
+            {
+                Console.WriteLine("Kérek több számot: ");
+                var szam = int.Parse(Console.ReadLine());
+                if (szam != null)
+                {
+                    Console.WriteLine("Megadott szám: " + szam.ToString());
+                    Összeg += szam;
+                    Console.WriteLine("Összeg: " + Összeg);
+                    if (Összeg >= 100)
+                    {
+                        Console.WriteLine("A megadott számok összege több vagy egyenlő mint száz!");
+                        Console.ReadKey();
+                        break;
+
+
+
+
+
+
+                    }
+                }
+
+
+
+            }
+        }
+    }
+
+}
+
